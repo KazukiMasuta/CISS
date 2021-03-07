b63481dd0274c89360b332d2b5c567e188f1534d
BasyaKuE <49632170+basyakue@users.noreply.github.com>
2021-03-07 22:37:01 +0900

    Update admin.py
    add Vote

--- cissapp/admin.py
+++ cissapp/admin.py
@@ -1,8 +1,10 @@
 
 from django.contrib import admin
 
-from cissapp.models import Data, Topic
+from cissapp.models import Data, Topic, Vote
 
 admin.site.register(Data)
 
 admin.site.register(Topic)
+
+admin.site.register(Vote)
